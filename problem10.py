8207ce4d8dd313aa119b1e63170e8ee440e5cb9e
Sergey Lisitsin <sergey.lisitsin@gmail.com>
2019-05-28 09:55:18 +0100

    Create problem10.py
    Solves problem 10 at https://projecteuler.net

---
+++ problem10.py
@@ -0,0 +1,17 @@
+def findprimes(count):
+    ''' This function returns first count
+    number of primes'''
+    primes = [2,3,5,7]
+    start = 9
+    for x in range(start, count):
+        isprime = True
+        for p in primes:
+            if x % p == 0:
+                isprime = False
+                break
+        if isprime:
+            primes.append(x)
+    return primes
+
+list = findprimes(2000000)
+sum(list)
