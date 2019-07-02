1600183e7e286d01fc7cd23c93c041b9d4957087
Sergey Lisitsin <sergey.lisitsin@sicl.com>
2019-07-02 17:50:07 +0100

    Added problem_44

---
+++ problem_44.py
@@ -0,0 +1,23 @@
+''' Solves problem #44 on https://projecteuler.net
+Sergey Lisitsin. July 2019'''
+
+def pents(count):
+    result = [1]
+    s = 2
+    while len(result) < count:
+        next = s*(s*3 - 1)/2
+        result.append(int(next))
+        s+=1
+    return result
+
+pens = pents(10000)
+pairs = set()
+
+for x in pens:
+    for y in pens:
+        if x == y:
+            continue
+        if x+y in pens and abs(x - y) in pens:
+            print(abs(x-y))
+            break
+            
