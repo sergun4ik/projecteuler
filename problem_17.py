f02667b1b9c168126a2c64f3385463aa9533211e
sergun4ik <sergey.lisitsin@gmail.com>
2019-12-30 12:44:20 +0000

    Finished problem_17

--- problem_17.py
+++ problem_17.py
@@ -1,16 +1,46 @@
 ''' Solves problem #17 on https://projecteuler.net
 Sergey Lisitsin. June 2019'''
 
-def hundredand(hundred):
-    result = ''
-    for x in range(1,10):
-        
+def get_tail(num):
+    output = 'and'
+    startlist = 'zero one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen'.split()
+    tenslist = 'zero ten twenty thirty forty fifty sixty seventy eighty ninety'.split()
+    tail = str(num)[-2::]
+    if tail == '00':
+        return ''
+    if len(str(num)) < 2:
+        return startlist[(int(num))]
+    if len(str(num)) == 2:
+        output = ''
+    if tail[-1] == '0':
+        output = output + str(tenslist[(int(tail[0]))])
+        return output
+    if tail[0] == '0':
+        output = output + str(startlist[(int(tail[1]))])
+        return output
+    if tail[0] == '1':
+        output = output + str(startlist[int(tail)])
+        return output
+    if '0' not in tail:
+        output = output + str(tenslist[(int(tail[0]))]) + str(startlist[(int(tail[1]))])
+    return output
+
+def get_hund(num):
+    startlist = 'zero one two three four five six seven eight nine'.split()
+    if len(str(num)) > 2:
+        return startlist[int(str(num)[0])] + 'hundred'
 
 
-startlist = 'one two three four five six seven eight nine ten eleven twelve thirteen fourteen fifteen sixteen seventeen nineteen'.split()
-tenslist = 'twenty thirty forty fifty sixty seventy eighty ninety'.split()
+def wordise(num):
+    if get_hund(num):
+        return get_hund(num)+get_tail(num)
+    else:
+        return get_tail(num)
 
-for x in range(10):
-    for y in range(10):
-        for z in range(10):
-            print
+
+master = ''
+for x in range(1,1000):
+    print(wordise(x))
+    master = master + wordise(x)
+master = master + 'onethousand'
+print(len(master))
