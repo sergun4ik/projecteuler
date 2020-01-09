fc8b01f980194c3d258d37d96272bdac49db3775
sergun4ik <sergey.lisitsin@gmail.com>
2020-01-09 12:30:50 +0000

    Finished problem_46

--- problem_46.py
+++ problem_46.py
@@ -7,6 +7,32 @@ def isprime(num):
             return False
     return True
 
+def checkcubesum(prime,num):
+    start = 1
+    while prime + 2 * start**2 < num:
+        start += 1
+        if prime + 2 * start ** 2 == num:
+            return True
+    return False
+
 def goldbach(num):
     smallerprimes = []
-    for x in range(1,num):
+    for x in range(2,num):
+        if isprime(x):
+            smallerprimes.append(x)
+    #print(smallerprimes)
+    gold = False
+    for x in smallerprimes:
+        if checkcubesum(x,num):
+            return True
+    return False
+
+p = 5001
+while goldbach(p):
+    p+=2
+    if isprime(p):
+        continue
+
+print(p)
+
+
