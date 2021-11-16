243aea89d0cd8cc9a27a72395c90861fd59fc0ed
sergun4ik <sergey.lisitsin@gmail.com>
2021-11-16 12:44:34 +0000

    Modified problem_315.py - not solved yet

--- problem_315.py
+++ problem_315.py
@@ -3,83 +3,64 @@ Sergey Lisitsin. Apr 2020
 '''
 
 
-digs =  [126, 12, 91, 31, 45, 55, 60, 28, 127, 63]
+zero = [1,1,1,1,1,1,0]
+one = [0,0,0,1,1,0,0]
+two = [1,0,1,1,0,1,1]
+three = [0,0,1,1,1,1,1]
+four = [0,1,0,1,1,0,1]
+five = [0,1,1,0,1,0,1]
+six = [1,1,1,0,1,1,1]
+seven = [0,1,1,1,1,0,0]
+eight = [1,1,1,1,1,1,1]
+nine = [0,1,1,1,1,1,1]
+off = [0,0,0,0,0,0,0]
+digits = [zero, one, two, three, four, five, six, seven, eight, nine, off]
+
+def digit_switch(dig):
+    return(dig.count(1))
 
 def digit_transition(dig1,dig2):
-    if dig1 == '*':
-        result = 0 ^ digs[dig2]
-    elif dig2 == '*':
-        result = digs[dig1] ^ 0
-    else:
-        result = digs[dig1] ^ digs[dig2]
-    count = str(bin(result)).count('1')
+    d1arr = digits[int(dig1)]
+    d2arr = digits[int(dig2)]
+    count = 0
+    for x,y in zip(d1arr,d2arr):
+        count += x ^ y
     return count
 
-def max_transition(n1, n2):
-    if n1 == 0:
-        string1 = ''
-        string2 = str(n2)
-    elif n2 == 0:
-        string2 = ''
-        string1 = str(n1)
-    else:
-        string1 = str(n1)
-        string2 = str(n2)
-    if len(string1) > len(string2):
-        dif = len(string1)-len(string2)
-        filler = '*'*dif
-        string2 = filler+string2
-    elif len(string2) > len(string1):
-        dif = len(string2)-len(string1)
-        filler = '*'*dif
-        string1 = filler + string1
+
+def max_transition(n1,n2):
     counter = 0
-    for x,y in zip(string1, string2):
-        if x == '*':
-            counter += digit_transition('*',int(y))
-        elif y == '*':
-            counter += digit_transition(int(x),'*')
-        else:
-            counter += digit_transition(int(x),int(y))
+    n1arr = [int(x) for x in str(n1)]
+    n2arr = [int(x) for x in str(n2)]
+    n1len = len(n1arr)
+    n2len = len(n2arr)
+    dif = abs(n1len-n2len)
+    while n2len < n1len:
+        n2arr.insert(0,10)
+        n2len = len(n2arr)
+    while n1len < n2len:
+        n1arr.insert(0,10)
+        n1len = len(n1arr)
+    print(n1arr,n2arr)
+    for x,y in zip(n1arr,n2arr):
+        counter += digit_transition(x,y)
     return counter
 
-def reduce_number(n):
-    if len(str(n)) > 1:
-        l = [int(x) for x in str(n)]
-        count = 0
-        for x in l:
-            count += x
-        return count
-    else:
-        return n
 
+    def max_transition(n1, n2):
+    n1arr = [digits[int(x)] for x in str(n1)]
+    n2arr = [digits[int(x)] for x in str(n2)]
+    n1diff = 3 - len(n1arr)
+    n2diff = 3 - len(n2arr)
+    for x in range(n1diff):
+        n1arr.insert(0,off)
+    for x in range(n2diff):
+        n2arr.insert(0,off)
+    for x,y in zip (n1arr,n2arr):
+        print(digit_transition(x,y))
 
-def root_chain(num):
-    chain = [num]
-    while len(str(num)) > 1:
-        num = reduce_number(num)
-        chain.append(num)
-    chain.append(0)
-    return chain
+def sams_number(num):
+    n1arr = [digits[int(x)] for x in str(num)]
+    n2arr = [off] * len(n1arr)
+    return digit_transition(n1arr, n2arr)
 
-def max_transitions(num):
-    counter = 0
-    while num > 0:
-        reduced = reduce_number(num)
-        counter+= max_transition(num,reduced)
-        num = reduced
-    counter += max_transition(num,'*')
-    return counter
-
-def sam_transition(n1):
-    counter = 0
-    string = str(n1)
-    for x in string:
-        counter += digit_transition(int(x),'*')
-    return counter*2
-
-chain = root_chain(137)
-count = 0
-for x in range(len(root_chain)-1):
-    pair = (chain[x],chain[x]+1)
-    count +=
